59010f6e00718cee6a59fbd93cbdc5dce8ecd161
André Furlan <ensismoebius@gmail.com>
2018-07-30 20:08:18 -0300

    Aula de 30 de jul

--- 01- VariaveisEFormularios/index.php
+++ 01- VariaveisEFormularios/index.php
@@ -5,6 +5,13 @@
 <title>Form</title>
 </head>
 <body>
+    <h1>Repete via GET</h1>
+	<form action="processa.php" method="get">
+		<input type="text" name="qtde"/>
+		<input type="submit" />
+	</form>
+
+
     <h1>Manda via GET</h1>
 	<form action="processa.php" method="get">
 		<input type="text" name="cpf"/>
@@ -14,7 +21,7 @@
 	</form>
 
     <h1>Manda via POST</h1>
-	<form action="processa.php" method="get">
+	<form action="processa.php" method="post">
 		<input type="text" name="cpf"/>
 		<input type="text" name="rg"/>
 		<input type="text" name="end"/>
--- 01- VariaveisEFormularios/processa.php
+++ 01- VariaveisEFormularios/processa.php
@@ -1,4 +1,25 @@
 <?php
+function repete3Vezes($quantidade) {
+	// Usando o for
+	for($i = 0; $i < $quantidade; $i ++) {
+		echo $i + 1 . " - Olá<br>";
+	}
+	
+	// Usando o while
+	$contador = 0;
+	
+	while ( $contador < $quantidade ) {
+		echo $contador + 1 . " - Olá<br>";
+		$contador ++;
+	}
+	
+	$contador = 0;
+	
+	do {
+		echo $contador + 1 . " - Olá<br>";
+		$contador ++;
+	} while ( $contador < $quantidade );
+}
 
 // Para criar uma variável em php basta escrever a variável
 
@@ -8,11 +29,32 @@ $variavelNumerica = 10;
 // Cria uma variável para texto (ou seja, string)
 $variavelDeTexto = "Olá mundo!";
 
-// Recupera os dados enviados via POST
-$mensagem = "Olá dona ou dono do cpf: " . $_POST ["cpf"] . " você ganhou um iphone!";
-echo $mensagem;
+if (isset ( $_GET ["qtde"] )) {
+	
+	// Chama a função usando os dados do GET
+	repete3Vezes ( $_GET ["qtde"] );
+	
+	// Chama a função usando um valor literal
+	repete3Vezes ( 47 );
+	return;
+}
 
-// Recupera os dados enviados via GET
-$mensagem = "Olá dona ou dono do cpf: " . $_GET ["cpf"] . " você ganhou um iphone!";
-echo $mensagem;
+/*
+ * Verifica se o cpf existe no POST ou existe no GET
+ * Caso não exista exibe uma mensagem informando o usuário
+ */
+if (isset ( $_POST ["cpf"] ) || isset ( $_GET ["cpf"] )) {
+	
+	if (isset ( $_POST ["cpf"] )) {
+		// Recupera os dados enviados via POST
+		$mensagem = "Olá dona ou dono do cpf: " . $_POST ["cpf"] . " você ganhou um iphone!";
+		echo $mensagem;
+	}
+	if (isset ( $_GET ["cpf"] )) { // Recupera os dados enviados via GET
+		$mensagem = "Olá dona ou dono do cpf: " . $_GET ["cpf"] . " você ganhou um iphone!";
+		echo $mensagem;
+	}
+} else {
+	echo "Você não enviou nada!";
+}
 ?>
